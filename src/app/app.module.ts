be0b250ea92dba100e7bd58cd794b8b52eb75643
Losanes <losanes.cris@iisgalvanimi.edu.it>
2022-02-10 08:34:15 +0000

    Setup iniziale e Creazione dei componenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Componente01Component } from './componente01/componente01.component';
+import { Componente02Component } from './componente02/componente02.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente01Component,
+    Componente02Component
   ],
   imports: [
     BrowserModule
